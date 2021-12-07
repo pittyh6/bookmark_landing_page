bfb5a15bd877b15ca756c2a6c138443aa42246e4
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2021-12-07 20:02:13 +1100

    [WDB-411][WDB-412] question and answer click=>   hide and show elements according click

--- bookmark_landing_page_project/index.js
+++ bookmark_landing_page_project/index.js
@@ -13,3 +13,16 @@ $("#simpleBookmarkinLink").on("click", function(){
     $("#speedySearching").hide();
     $("#easySharing").hide();
 });
+
+
+
+$("#q1").on("click", function(){
+    if($("#q1").hasClass("svg") === false){
+        $("#q1a").show();
+        $("#q1").addClass("svg");
+    }else{
+        $("#q1a").hide();
+        $("#q1").removeClass("svg");
+    }
+    
+});
