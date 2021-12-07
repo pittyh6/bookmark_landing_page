52c18fa568d334bea89518a7b6b3b01ded3665c0
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2021-12-08 06:07:48 +1100

    [WDB-361][WDB-360][WDB-359][WDB-357][WDB-358] format email, wrong message and success

--- bookmark_landing_page_project/index.js
+++ bookmark_landing_page_project/index.js
@@ -1,32 +1,49 @@
 // Features SHOW/HIDE
-$("#features a").on("click", function(event){
+$("#features a").on("click", function (event) {
     var textEvent = event.target.text;
-    if(textEvent === "Simple Bookmarking"){
+    if (textEvent === "Simple Bookmarking") {
         $("#simpleBookmarking").show();
         $("#speedySearching").hide();
         $("#easySharing").hide();
-    }else if(textEvent === "Speedy Searching"){
+    } else if (textEvent === "Speedy Searching") {
         $("#simpleBookmarking").hide();
         $("#easySharing").hide();
         $("#speedySearching").show();
-    }else if(textEvent === "Easy Sharing"){
+    } else if (textEvent === "Easy Sharing") {
         $("#simpleBookmarking").hide();
         $("#easySharing").show();
         $("#speedySearching").hide();
-    }else{
+    } else {
         console.log("Feature links is not working: " + event.target.text);
     }
 });
 
 // Questions SHOW/HIDE 
-$("#questions img").on("click", function(event){
+$("#questions img").on("click", function (event) {
     var idImg = event.target.id;
     console.log(idImg)
-    if($("#"+idImg).hasClass("svg") === false){
-        $("#"+idImg+"a").show();
-        $("#"+idImg).addClass("svg");
-    }else{
-        $("#"+idImg+"a").hide();
-        $("#"+idImg).removeClass("svg");
-    }    
+    if ($("#" + idImg).hasClass("svg") === false) {
+        $("#" + idImg + "a").show();
+        $("#" + idImg).addClass("svg");
+    } else {
+        $("#" + idImg + "a").hide();
+        $("#" + idImg).removeClass("svg");
+    }
 });
+
+//EMAIL validation
+var joinInputText = $("#join input");
+function EmailValidation(enteredEmail) {
+    var mail_format = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (enteredEmail.value.match(mail_format)) {
+        (joinInputText).addClass("is-valid");
+        $(joinInputText).removeClass("is-invalid");
+        document.form1.text1.focus();
+        return true;
+    }else{
+        $(joinInputText).addClass("is-invalid");        
+        $(joinInputText).removeClass("is-valid");
+        document.form1.text1.focus();
+        return false;
+    }
+}
